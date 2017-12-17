a2e52f9768982cb83e2a2084b29ccc678ec9feb0
wanghao <wanghao6611@gmail.com>
2017-12-18 03:13:05 +0900

    dev has been merged with master

--- src/test/hello.java
+++ src/test/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("i just want to add sth new");
-		System.out.println("i just want to test dev branch ");
+		System.out.println("i just want to test dev branch");
 	}
 
 }
